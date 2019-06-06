85718de6028e7f3b15a42c41261105ebfc47ffdc
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2019-06-06 23:54:54 +0100

    Create create_user_db.sh

---
+++ create_user_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+input="users_and_passwords.txt"
+output="users_and_passwords_sha512.txt"
+while IFS=: read -r f1 f2
+do
+  HASH=`mkpasswd $f2 -m sha-512` # verschlüsseltes Passwort erzeugen
+  echo "$f1" >> $output
+  echo "$HASH" >> $output
+done < "$input"
+# Datenbank erstellen
+db_load -T -f $output -t hash userpass.db
+sudo mv userpass.db /etc/userpass.db
+# Besitzer und Zugriffsrechte ändern
+chown root:root /etc/userpass.db
+chmod 0600 /etc/userpass.db
